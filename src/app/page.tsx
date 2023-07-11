9b72d743bd60c98e7f5903b71e0e5a5a07c91f2d
Louis Bompart <lbompart@coveo.com>
2023-07-11 16:49:51 -0400

    cookies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
-import { ResultList } from "@/app/components/result-list.fn";
-import {
-  buildSearchEngine,
-  getSampleSearchEngineConfiguration,
-  SearchEngine,
-} from "@coveo/headless";
-import SearchPage from "./components/search-page";
-import { SearchBox } from "./components/search-box.fn";
-
-const getEngine = async (): Promise<SearchEngine> => {
-  const engine = buildSearchEngine({
-    configuration: getSampleSearchEngineConfiguration(),
-  });
-  engine.executeFirstSearch();
-  return new Promise<SearchEngine>((resolve) => {
-    engine.subscribe(() => {
-      if (engine.state.search.searchResponseId) {
-        resolve(engine);
-      }
-    });
-  });
-};
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const engine = await getEngine();
+  const cookieValue = JSON.stringify(globalThis.toString() === '[object Window]' ? document.cookie: cookies().getAll())
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello world!</h1>
-      <SearchPage state={engine.state}>
-        <SearchBox></SearchBox>
-        <ResultList></ResultList>
-      </SearchPage>
+      <p>The cookie value is {cookieValue}</p>
     </main>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookieName = "myawesomecookie";
+  let hadCookie = request.cookies.has(cookieName);
+  if (hadCookie) {
+    let cookie = request.cookies.get(cookieName);
+    console.log(cookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
+  } else {
+    request.cookies.set(cookieName, "potato");
+  }
+
+  // Setting cookies on the response using the `ResponseCookies` API
+  const response = NextResponse.rewrite(new URL("/", request.url))
+  if (!hadCookie) {
+    response.cookies.set(cookieName, "potato");
+  }
+
+  return response;
+}
